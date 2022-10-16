645df3701b0b56e42d97d56e03e67f374ce30951
as00r <mohitkik2@gmail.com>
2022-10-17 01:42:19 +0530

    create footer.ejs and header.ejs file

--- app.js
+++ app.js
@@ -5,35 +5,45 @@ const bodyParser = require('body-parser')
   app.use(express.static("public"));
 const ejs = require('ejs')
   app.set("view engine", "ejs");
+const date = require(__dirname + "/public/js/date.js");
 
-let inputs = ["Wake Up", "Do daily morning routine", "Have Tea"];
+const inputs = ["Wake Up", "Do daily morning routine", "Have Tea"];
+const works = [];
 
-// route for index page
+// route for home page
 app.get("/", function (req, res) {
-  let today = new Date();
-  let options = {
-    weekday : "long",
-    day : "numeric",
-    month : "long"
-  }
-
-  // logic for getting Day name
-  let day = today.toLocaleDateString("en-US", options);
-
+  const day = date.getDate();
   // Render list.ejs and with the value of current day
   res.render("list", {
-    kindOfDay: day,
+    titleName: day,
     newItems: inputs
+  });
+
+});
+
+// route for work page
+app.get("/work", function (req, res) {
+  // Render list.ejs and with the value of work
+  res.render("list", {
+    titleName: "Work",
+    newItems: works
   });
 });
 
 // handle POST request from form
 app.post("/", function (req, res) {
-  let userInputVal = req.body.userInput;
+  const userInputVal = req.body.userInput;
 
-  inputs.push(userInputVal)
-  res.redirect("/")
+  if(req.body.list === "Work") {
+    works.push(userInputVal);
+    res.redirect("/work");
+  } else {
+    inputs.push(userInputVal);
+    res.redirect("/");
+  }
+
 })
+
 
 // Listen to connections on port 8080
 app.listen(8080, function () {
