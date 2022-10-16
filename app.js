3ecc848b0626b57166d58931603f52c2e71cc7fb
as00r <mohitkik2@gmail.com>
2022-10-17 00:45:17 +0530

    make working todolist in home route

--- app.js
+++ app.js
@@ -2,13 +2,38 @@ const express = require('express')
   app = express();
 const bodyParser = require('body-parser')
   app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(express.static("public"));
 const ejs = require('ejs')
   app.set("view engine", "ejs");
 
+let inputs = ["Wake Up", "Do daily morning routine", "Have Tea"];
+
 // route for index page
 app.get("/", function (req, res) {
-  res.render("index");
+  let today = new Date();
+  let options = {
+    weekday : "long",
+    day : "numeric",
+    month : "long"
+  }
+
+  // logic for getting Day name
+  let day = today.toLocaleDateString("en-US", options);
+
+  // Render list.ejs and with the value of current day
+  res.render("list", {
+    kindOfDay: day,
+    newItems: inputs
+  });
 });
+
+// handle POST request from form
+app.post("/", function (req, res) {
+  let userInputVal = req.body.userInput;
+
+  inputs.push(userInputVal)
+  res.redirect("/")
+})
 
 // Listen to connections on port 8080
 app.listen(8080, function () {
