bd0e6420ba1c8b4e5c73c8f400f278481ed91eb2
as00r <mohitkik2@gmail.com>
2022-10-17 12:54:48 +0530

    add procfile and change listen code in app.js

--- app.js
+++ app.js
@@ -49,6 +49,6 @@ app.post("/", (req, res) =>  {
 
 
 // Listen to connections on port 8080
-app.listen(8080, () => {
-  console.log("Server is running on port 8080 ");
+app.listen(process.env.PORT || 8080, () => {
+  console.log("Server is running...");
 });
